029369b89c0fd444753a01d040c2f67d6c8fa318
Sanzhar111 <sanzhar_serikovich@mail.ru>
2022-07-19 17:18:10 +0600

    Working project

---
+++ Food Application/Componets/ProductListCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductListCollectionViewCell.swift
+//  Food Application
+//
+//  Created by Admin on 17.07.2022.
+//
+
+import UIKit
+
+class ProductListCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Food Application/Utilities/Utilities.swift
@@ -0,0 +1,8 @@
+//
+//  Utilities.swift
+//  Food Application
+//
+//  Created by Admin on 13.07.2022.
+//
+
+import Foundation
